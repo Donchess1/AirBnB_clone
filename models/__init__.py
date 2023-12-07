7d60ea00c1aa5c92eac16f01a966e31f509253a5
Donchess1 <stepabod@yahoo.com>
2023-12-07 21:48:26 +0000

    shot5

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import file_storage.py
+
+fileStorage.storage=reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,17 +3,23 @@
 
 from models.base_model1 import BaseModel
 import json
+
+
 class FileStorage(BaseModel):
     """filestorage class"""
-    def __init__(self, _file_path=None, _objects=None):
-        self._file_path = _file_path
-        self._objects = _objects
+    def __init__(self, __file__path=None, __objects={}):
+        self.__file__path = __file__path
+        self.__objects = _objects
 
-    def _objects(self):
-        objects = "{}.{}".format(self.__class__.__name__, self.id)
-        return {objects, value}
+    def all(self):
+        __objects = "{}.{}".format(self.__class__.__name__, self.id)
+        return {__objects, value}
+    def new(self,obj):
+        obj = "obj{}.{}".format(self.__class__.__name__, self.id)
+    
+    def save(self):
+        json.dump(__objects, __file__path, indent=4)
 
-    def _file_path(self):
-        file_path = os.path.dirname(os.path.realpath(__file__))
-        return 
-
+    def reload(self):
+        if __file__path:
+            return json.load(__objects)
